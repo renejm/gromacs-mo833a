22db07c286b788d13c92cfe487ae83299cefb378
Rene Juliano Martins <rene@ggaunicamp.com>
2020-05-17 21:12:06 -0300

    Update scripts

---
+++ experiments/ativ-7-exp-1/runexp4x.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\e[92m\e[1m********************************************************"
+echo -e "*************** EXPERIMENTO COM 4 NÓS... ***************"
+echo -e "********************************************************\e[0m"
+echo " "
+clapp cluster start cluster-t2.micro-4x
+clusterid=$(clapp cluster list | grep id | cut -d' ' -f2)
+nodeid=$(clapp node list | grep node-master | cut -d' ' -f3 | cut -d'=' -f2 | cut -d'`' -f2)
+echo "Cluster id: $clusterid"
+echo "Node id:    $nodeid"
+clapp cluster action $clusterid gromacs run --nodes $nodeid
+clapp cluster action $clusterid gromacs fetch-result --nodes $nodeid
+echo " "
+echo -e "\e[92m\e[1m********************************************************"
+echo -e "*********** EXPERIMENTO COM 4 NÓS CONCLUÍDO! ***********"
+echo -e "********************************************************\e[0m"
+echo " "
